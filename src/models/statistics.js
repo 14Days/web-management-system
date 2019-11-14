0d303c13760085e25145736eb24996f1beb258c0
BLANKMYMIND <zhangshunyucn@qq.com>
2019-11-14 17:46:30 +0800

    [fit] 统计页重构布局，对接mock

--- src/models/statistics.js
+++ src/models/statistics.js
@@ -3,32 +3,69 @@ import { getRecord } from '../services/statistics'
 const StatisticsModel = {
   namespace: 'statistics',
   state: {
-    last: Date(),
-    loading: false,
-    interval: 1,
+    last: Date(), // 更新时间（拉取时间的截止时间）
+    loading: false, // 是否处于加载状态
+    interval: 1, // 当前查询的时间间隔
     numNewuser: 1,
     numNewrecommend: 1,
-    nameStylelike: 'fengge',
-    numStylelike: 1,
-    nameStylecollect: 'fengge',
-    numStylecollect: 1,
-    nameStylecomment: 'fengge',
-    numStylecomment: 1,
-    nameDesignpost: 'desi',
-    numDesignpost: 1,
-    nameDesignfollow: 'desi',
-    numDesignfollow: 1,
-    nameDesigncomment: 'desi',
-    numDesigncomment: 1,
-    nameDesignlike: 'desi',
-    numDesignlike: 1,
+    styleLike: [],
+    styleCollect: [],
+    styleComment: [],
+    designerPost: [],
+    designerFollow: [],
+    designerComment: [],
+    designerLike: [],
   },
   reducers: {
+    // 直接存储
     save(state, { payload }) {
       return {
         ...state,
         ...payload,
       };
+    },
+    // 添加 key 键并存储
+    update(state, { payload }) {
+      // 为数组内每一元素添加 key 键， 该键用于排名
+      const styleLike = payload.styleLike.map((ele, index) => ({
+        ...ele,
+        key: index + 1,
+      }));
+      const styleCollect = payload.styleCollect.map((ele, index) => ({
+        ...ele,
+        key: index + 1,
+      }));
+      const styleComment = payload.styleComment.map((ele, index) => ({
+        ...ele,
+        key: index + 1,
+      }));
+      const designerPost = payload.designerPost.map((ele, index) => ({
+        ...ele,
+        key: index + 1,
+      }));
+      const designerFollow = payload.designerFollow.map((ele, index) => ({
+        ...ele,
+        key: index + 1,
+      }));
+      const designerComment = payload.designerComment.map((ele, index) => ({
+        ...ele,
+        key: index + 1,
+      }));
+      const designerLike = payload.designerLike.map((ele, index) => ({
+        ...ele,
+        key: index + 1,
+      }));
+      return {
+        ...state,
+        ...payload,
+        styleLike,
+        styleCollect,
+        styleComment,
+        designerPost,
+        designerFollow,
+        designerComment,
+        designerLike,
+      }
     },
   },
   effects: {
@@ -40,46 +77,34 @@ const StatisticsModel = {
           loading: true,
         },
       });
-      // call api
-      const dateNow = Date();
+      // 计算日期
+      const dayEnd = new Date();
       const { interval } = yield select(state => state.statistics);
-      const dateEnd = dateNow + interval;
-      console.log('daend:', dateEnd);
+      const dayStart = new Date();
+      dayStart.setDate(dayEnd.getDate() - interval);
+      // 调用 api
       const res = yield call(getRecord, {
-        start_date: dateNow,
-        end_date: dateEnd,
+        start_date: dayStart,
+        end_date: dayEnd,
       });
       if (res.status === 'success') {
         console.log('okk');
       }
-      // yield call(() => {
-      //   return new Promise(resolve => {
-      //     setTimeout(resolve, 1000);
-      //   });
-      // });
-      // set date
-      console.log(res);
+      // 调用update更新state（添加key键（用于排名））
       yield put({
-        type: 'save',
+        type: 'update',
         payload: {
-          last: dateNow,
+          last: dayEnd.toString(),
           loading: false,
           numNewuser: res.data.register,
           numNewrecommend: res.data.recommend,
-          nameStylelike: res.data.style.like.tag,
-          numStylelike: res.data.style.like.num,
-          nameStylecollect: res.data.style.collect.tag,
-          numStylecollect: res.data.style.collect.num,
-          nameStylecomment: res.data.style.comment.tag,
-          numStylecomment: res.data.style.comment.num,
-          nameDesignpost: res.data.designer.post.name,
-          numDesignpost: res.data.designer.post.num,
-          nameDesignfollow: res.data.designer.follow.name,
-          numDesignfollow: res.data.designer.follow.num,
-          nameDesigncomment: res.data.designer.comment.name,
-          numDesigncomment: res.data.designer.comment.num,
-          nameDesignlike: res.data.designer.like.name,
-          numDesignlike: res.data.designer.like.num,
+          styleLike: res.data.style.like,
+          styleCollect: res.data.style.collect,
+          styleComment: res.data.style.comment,
+          designerPost: res.data.designer.post,
+          designerFollow: res.data.designer.follow,
+          designerComment: res.data.designer.comment,
+          designerLike: res.data.designer.like,
         },
       });
     },
--- src/pages/statistics/index.jsx
+++ src/pages/statistics/index.jsx
@@ -1,4 +1,4 @@
-import { Col, Icon, Row, Card, Statistic, Radio, Tooltip, Descriptions, Table, Tag } from 'antd';
+import { Col, Icon, Row, Card, Statistic, Radio, Tooltip, Descriptions, Table } from 'antd';
 import React, { Component } from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { connect } from 'dva';
@@ -10,42 +10,6 @@ import Rank from './Rank';
 }))
 class Statistics extends Component {
   state = {};
-
-  columns = [
-    {
-      title: '排名',
-      dataIndex: 'key',
-      key: 'rank',
-    },
-    {
-      title: '设计师昵称',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: '受点赞数',
-      dataIndex: 'num',
-      key: 'num',
-    },
-  ];
-
-  data = [
-    {
-      key: '1',
-      name: 'nihao',
-      num: 23,
-    },
-    {
-      key: '2',
-      name: 'sobuhao',
-      num: 24,
-    },
-    {
-      key: '3',
-      name: 'niao',
-      num: 25,
-    },
-  ];
 
   componentWillMount() {
     const { dispatch } = this.props;
@@ -62,21 +26,148 @@ class Statistics extends Component {
       interval,
       numNewuser,
       numNewrecommend,
-      nameStylelike,
-      numStylelike,
-      nameStylecollect,
-      numStylecollect,
-      nameStylecomment,
-      numStylecomment,
-      nameDesignpost,
-      numDesignpost,
-      nameDesignfollow,
-      numDesignfollow,
-      nameDesigncomment,
-      numDesigncomment,
-      nameDesignlike,
-      numDesignlike,
+      styleLike,
+      styleCollect,
+      styleComment,
+      designerPost,
+      designerFollow,
+      designerComment,
+      designerLike,
     } = statistics; // 抽取数据
+
+    // 列参数
+    const styleLikeColumns = [
+      {
+        title: '排名',
+        dataIndex: 'key',
+        key: 'rank',
+        render: text => <span>{ `# ${text}` }</span>,
+      },
+      {
+        title: '风格',
+        dataIndex: 'tag',
+        key: 'name',
+      },
+      {
+        title: '点赞数',
+        dataIndex: 'num',
+        key: 'num',
+      },
+    ];
+
+    const styleCommentColumns = [
+      {
+        title: '排名',
+        dataIndex: 'key',
+        key: 'rank',
+        render: text => <span>{ `# ${text}` }</span>,
+      },
+      {
+        title: '风格',
+        dataIndex: 'tag',
+        key: 'name',
+      },
+      {
+        title: '评论数',
+        dataIndex: 'num',
+        key: 'num',
+      },
+    ];
+
+    const styleCollectColumns = [
+      {
+        title: '排名',
+        dataIndex: 'key',
+        key: 'rank',
+        render: text => <span>{ `# ${text}` }</span>,
+      },
+      {
+        title: '风格',
+        dataIndex: 'tag',
+        key: 'name',
+      },
+      {
+        title: '收藏数',
+        dataIndex: 'num',
+        key: 'num',
+      },
+    ];
+
+    const designerPostColumns = [
+      {
+        title: '排名',
+        dataIndex: 'key',
+        key: 'rank',
+        render: text => <span>{ `# ${text}` }</span>,
+      },
+      {
+        title: '设计师昵称',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '新增推荐数',
+        dataIndex: 'num',
+        key: 'num',
+      },
+    ];
+
+    const designerLikeColumns = [
+      {
+        title: '排名',
+        dataIndex: 'key',
+        key: 'rank',
+        render: text => <span>{ `# ${text}` }</span>,
+      },
+      {
+        title: '设计师昵称',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '新增被点赞数',
+        dataIndex: 'num',
+        key: 'num',
+      },
+    ];
+
+    const designerFollowColumns = [
+      {
+        title: '排名',
+        dataIndex: 'key',
+        key: 'rank',
+        render: text => <span>{ `# ${text}` }</span>,
+      },
+      {
+        title: '设计师昵称',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '新增被关注数',
+        dataIndex: 'num',
+        key: 'num',
+      },
+    ];
+
+    const designerCommentColumns = [
+      {
+        title: '排名',
+        dataIndex: 'key',
+        key: 'rank',
+        render: text => <span>{ `# ${text}` }</span>,
+      },
+      {
+        title: '设计师昵称',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '新增被评论数',
+        dataIndex: 'num',
+        key: 'num',
+      },
+    ];
 
     return (
       <PageHeaderWrapper
@@ -95,9 +186,7 @@ class Statistics extends Component {
                   },
                 });
               }}
-            >
-              近24时
-            </Radio.Button>
+            >近24时</Radio.Button>
             <Radio.Button
               value={7}
               onClick={() => {
@@ -108,9 +197,7 @@ class Statistics extends Component {
                   },
                 });
               }}
-            >
-              近7日
-            </Radio.Button>
+            >近7日</Radio.Button>
             <Radio.Button
               value={30}
               onClick={() => {
@@ -121,9 +208,7 @@ class Statistics extends Component {
                   },
                 });
               }}
-            >
-              近30日
-            </Radio.Button>
+            >近30日</Radio.Button>
           </Radio.Group>,
         ]}
         content={[
@@ -145,6 +230,8 @@ class Statistics extends Component {
           </Descriptions>,
         ]}
       >
+      {/* 页头声明结束 */}
+      {/* 具体内容开始 */}
         <Row gutter={24} type="flex">
           {/* 每个分组是一个card */}
           <Col xl={24} lg={24} md={24} sm={24} xs={24}>
@@ -169,46 +256,75 @@ class Statistics extends Component {
               </Row>
             </Card>
           </Col>
+          {/* 每一卡片 Rank展示前三 Table展示后七 */}
           <Col xl={8} lg={24} md={24} sm={24} xs={24}>
-            <Card title='风格点赞排名' className={styles.infoCard} loading={loading} bordered={false}>
-              <Rank hello={123}/>
-              <Table columns={this.columns} dataSource={this.data} pagination={false}/>
+            <Card title="风格点赞排名" className={styles.infoCard} loading={loading} bordered={false}>
+              {/* Rank：主体type 0风格 1设计师; 类型kind 内容类型 */}
+              <Rank type={0} data={styleLike} kind="like"/>
+              <Table columns={styleLikeColumns}
+                dataSource={styleLike.slice(3, 10)}
+                pagination={false}
+              />
             </Card>
           </Col>
           <Col xl={8} lg={24} md={24} sm={24} xs={24}>
-            <Card title='风格评论排名' className={styles.infoCard} loading={loading} bordered={false}>
-              <Rank hello={123}/>
-              <Table columns={this.columns} dataSource={this.data} pagination={false}/>
+            <Card title="风格收藏排名" className={styles.infoCard} loading={loading} bordered={false}>
+              <Rank type={0} data={styleCollect} kind="collect"/>
+              <Table
+                columns={styleCollectColumns}
+                dataSource={styleCollect.slice(3, 10)}
+                pagination={false}
+              />
             </Card>
           </Col>
           <Col xl={8} lg={24} md={24} sm={24} xs={24}>
-            <Card title='风格收藏排名' className={styles.infoCard} loading={loading} bordered={false}>
-              <Rank hello={123}/>
-              <Table columns={this.columns} dataSource={this.data} pagination={false}/>
+            <Card title="风格评论排名" className={styles.infoCard} loading={loading} bordered={false}>
+              <Rank type={0} data={styleComment} kind="comment"/>
+              <Table
+                columns={styleCommentColumns}
+                dataSource={styleComment.slice(3, 10)}
+                pagination={false}
+              />
             </Card>
           </Col>
           <Col xl={12} lg={24} md={24} sm={24} xs={24}>
-            <Card title='设计师发布排名' className={styles.infoCard} loading={loading} bordered={false}>
-              <Rank hello={123}/>
-              <Table columns={this.columns} dataSource={this.data} pagination={false}/>
+            <Card title="设计师点赞排名" className={styles.infoCard} loading={loading} bordered={false}>
+              <Rank type={1} data={designerLike} kind="like"/>
+              <Table
+                columns={designerLikeColumns}
+                dataSource={designerLike.slice(3, 10)}
+                pagination={false}
+              />
             </Card>
           </Col>
           <Col xl={12} lg={24} md={24} sm={24} xs={24}>
-            <Card title='设计师关注排名' className={styles.infoCard} loading={loading} bordered={false}>
-              <Rank hello={123}/>
-              <Table columns={this.columns} dataSource={this.data} pagination={false}/>
+            <Card title="设计师发布排名" className={styles.infoCard} loading={loading} bordered={false}>
+              <Rank type={1} data={designerPost} kind="post"/>
+              <Table
+                columns={designerPostColumns}
+                dataSource={designerPost.slice(3, 10)}
+                pagination={false}
+              />
             </Card>
           </Col>
           <Col xl={12} lg={24} md={24} sm={24} xs={24}>
-            <Card title='设计师评论排名' className={styles.infoCard} loading={loading} bordered={false}>
-              <Rank hello={123}/>
-              <Table columns={this.columns} dataSource={this.data} pagination={false}/>
+            <Card title="设计师关注排名" className={styles.infoCard} loading={loading} bordered={false}>
+              <Rank type={1} data={designerFollow} kind="follow"/>
+              <Table
+                columns={designerFollowColumns}
+                dataSource={designerFollow.slice(3, 10)}
+                pagination={false}
+              />
             </Card>
           </Col>
           <Col xl={12} lg={24} md={24} sm={24} xs={24}>
-            <Card title='设计师点赞排名' className={styles.infoCard} loading={loading} bordered={false}>
-              <Rank hello={123}/>
-              <Table columns={this.columns} dataSource={this.data} pagination={false}/>
+            <Card title="设计师评论排名" className={styles.infoCard} loading={loading} bordered={false}>
+              <Rank type={1} data={designerComment} kind="comment"/>
+              <Table
+                columns={designerCommentColumns}
+                dataSource={designerComment.slice(3, 10)}
+                pagination={false}
+              />
             </Card>
           </Col>
         </Row>
