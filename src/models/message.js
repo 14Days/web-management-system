28576a614f45475ebcfcb28c83038ddf6b463b9d
BLANKMYMIND <zhangshunyucn@qq.com>
2019-11-16 21:12:04 +0800

    [fix] 更改展示样式，新增大图查看器

--- src/models/message.js
+++ src/models/message.js
@@ -6,6 +6,7 @@ export default {
   state: {
     message: [],
     loading: true,
+    currentURL: '',
   },
   reducers: {
     save(state, { payload }) {
--- src/pages/message/messageManage.jsx
+++ src/pages/message/messageManage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { connect } from 'dva';
-import { Button, Carousel, List, Spin } from 'antd';
+import { Button, Tooltip, List, Spin, Card } from 'antd';
 
-import './message.less';
+import styles from './message.less';
 
 class Message extends React.Component {
   constructor(props) {
@@ -20,54 +20,94 @@ class Message extends React.Component {
 
   render() {
     return (
-      <PageHeaderWrapper>
-        <Spin spinning={this.props.loading}>
-          <List
-            itemLayout="vertical"
-            size="large"
-            dataSource={this.props.message}
-            pagination={{
-              pageSize: 5,
-            }}
-            footer={<div> i am footer!!!</div>}
-            renderItem={(item, index) => (
-              <List.Item
-                key="123"
-                extra={
-                  <div>
-                    <Carousel autoplay style={{ width: '450px' }}>
-                      {item.img_url.map(url => (
-                        <img src={url} alt={url} />
-                      ))}
-                    </Carousel>
-                    <Button
-                      type="danger"
-                      style={{
-                        position: 'reletive',
-                        bottom: '-50px',
-                      }}
-                      onClick={() => {
-                        this.props.dispatch({
-                          type: 'message/handleDelete',
-                          payload: {
-                            id: item.id,
-                            index,
-                          },
-                        });
-                      }}
-                    >
-                      删除
-                    </Button>
-                  </div>
-                }
-              >
-                <List.Item.Meta title={item.id} />
-                {item.content}
-              </List.Item>
-            )}
+      <React.Fragment>
+        {/* 大图查看器 */}
+        <div
+          className={styles.bigPhoto}
+          onClick={() => {
+            this.props.dispatch({
+              type: 'message/save',
+              payload: {
+                currentURL: '',
+              },
+            })
+          }}
+          style={{ display: this.props.currentURL === '' ? 'none' : 'flex' }}
+          id="bigPhotos"
+        >
+          <img
+            src={this.props.currentURL} alt={this.props.currentURL}
+            style={{ margin: 'auto' }}
           />
-        </Spin>
-      </PageHeaderWrapper>
+        </div>
+        {/* 页面部分 */}
+        <PageHeaderWrapper>
+          <Spin spinning={this.props.loading}>
+            <Card>
+              <List
+                itemLayout="vertical"
+                size="large"
+                dataSource={this.props.message}
+                pagination={{
+                  pageSize: 5,
+                }}
+                renderItem={(item, index) => (
+                  <List.Item
+                    key="123"
+                    extra={
+                      <div>
+                        <div style={{ width: '490px', display: 'flex', justifyContent: 'left' }}>
+                          {item.img_url.map(url => (
+                            <Tooltip
+                              title="查看大图"
+                              onClick={() => {
+                                this.props.dispatch({
+                                  type: 'message/save',
+                                  payload: {
+                                    currentURL: url,
+                                  },
+                                })
+                              }}
+                            >
+                              <img
+                                src={url} alt={url}
+                                style={{ width: '90px', height: '90px', objectFit: 'cover', margin: '10px' }}
+                              />
+                            </Tooltip>
+                          ))}
+                        </div>
+                        <div>
+                          <Button
+                            type="danger"
+                            style={{
+                              position: 'reletive',
+                              bottom: '-50px',
+                            }}
+                            onClick={() => {
+                              this.props.dispatch({
+                                type: 'message/handleDelete',
+                                payload: {
+                                  id: item.id,
+                                  index,
+                                },
+                              });
+                            }}
+                          >
+                            删除
+                          </Button>
+                        </div>
+                      </div>
+                    }
+                  >
+                    <List.Item.Meta title={item.id} />
+                    {item.content}
+                  </List.Item>
+                )}
+              />
+            </Card>
+          </Spin>
+        </PageHeaderWrapper>
+      </React.Fragment>
     );
   }
 }
