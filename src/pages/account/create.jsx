9da230d378a8e6e2395bb7eb69d492fb1f847ffe
qkuns <qkuns@qq.com>
2019-12-15 20:37:47 +0800

    修改样式try-catch

--- src/pages/account/create.jsx
+++ src/pages/account/create.jsx
@@ -3,6 +3,8 @@ import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { Form, Input, Button, Spin } from 'antd';
 import { connect } from 'dva';
 
+import styles from './index.less';
+
 class Create extends Component {
   constructor(props) {
     super(props);
@@ -33,29 +35,31 @@ class Create extends Component {
     return (
       <PageHeaderWrapper>
         <Spin spinning={this.props.loading.create}>
-          <Form.Item>
-            {getFieldDecorator('username', {
-              rules: [
-                {
-                  required: true,
-                  message: '请输入用户名！',
-                },
-              ],
-            })(<Input type="username" placeholder="请输入用户名" />)}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('password', {
-              rules: [
-                {
-                  required: true,
-                  message: '请输入密码！',
-                },
-              ],
-            })(<Input type="password" placeholder="请输入密码" />)}
-          </Form.Item>
-          <Button type="primary" onClick={this.commit}>
-            确认提交
-          </Button>
+          <div className={styles.container}>
+            <Form.Item className={styles.item}>
+              {getFieldDecorator('username', {
+                rules: [
+                  {
+                    required: true,
+                    message: '请输入用户名！',
+                  },
+                ],
+              })(<Input type="username" placeholder="请输入用户名" />)}
+            </Form.Item>
+            <Form.Item className={styles.item}>
+              {getFieldDecorator('password', {
+                rules: [
+                  {
+                    required: true,
+                    message: '请输入密码！',
+                  },
+                ],
+              })(<Input type="password" placeholder="请输入密码" />)}
+            </Form.Item>
+            <Button type="primary" onClick={this.commit} className={styles.item}>
+              确认提交
+            </Button>
+          </div>
         </Spin>
       </PageHeaderWrapper>
     );
